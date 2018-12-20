2d689838af06d8ff045e354b63eb92f68a42026a
Ana Laura Diedrichs <analauradiedrichs@gmail.com>
2018-12-20 18:28:05 +0000

    change group_by by dplyr::group_by and summarise_ by dplyr::summarise

--- shinyssd/inst/shiny/app.R
+++ shinyssd/inst/shiny/app.R
@@ -177,8 +177,8 @@ server <- function(input, output, session){
      lista <- data.frame(filtered()$sps_sc_name, filtered()$sps_group)
      colnames(lista) <- c("sps_sc_name", "sps_group")
      ul <- list %>%
-       group_by_(sps_sc_name, sps_group) %>%
-       summarise_()
+       dplyr::group_by(sps_sc_name, sps_group) %>%
+       dplyr::summarise()
      colnames(ul) <- c("sps_sc_name", "sps_group")
      ta <- merge(ul, ta, sort = FALSE, by.x = "sps_sc_name", by.y = "sps_sc_name" )
      colnames(ta) <- c("sps_sc_name", "sps_group", "values", "frac")
@@ -189,12 +189,12 @@ server <- function(input, output, session){
    visual <- reactive({
     visual <- tbl() %>%
       dplyr::filter(input$chem_name == chem_name) %>%
-      group_by_(sps_group, sps_sc_name, endpoint, chem_name) %>%
-      summarise_(n())
+      dplyr::group_by(sps_group, sps_sc_name, endpoint, chem_name) %>%
+      dplyr::summarise(n())
     colnames(visual)<-c("sps_group", "sps_sc_name", "endpoint", "chem_name", "n")
     vis <- visual %>%
-      group_by_(sps_group, endpoint) %>%
-      summarise_(n())
+      dplyr::group_by(sps_group, endpoint) %>%
+      dplyr::summarise(n())
     colnames(vis)<-c("sps_group", "endpoint", "n")
     vis$Y1 <- cut(vis$n, breaks = c(0, 8, 10, 30, Inf), right = FALSE)
     print(vis)
